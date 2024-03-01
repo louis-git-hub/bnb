afeab1a53fac38c437017f9ce853e21a58f3c772
louis-git-hub <107806927+louis-git-hub@users.noreply.github.com>
2024-03-01 18:39:45 +0100

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -1,10 +1,11 @@
 import sqlite3
 import subprocess
 import time
-
+import requests
 
 # Chemin vers le script secondaire que vous souhaitez exécuter
 script_path = 'airbnb.py'
+webhook_url = 'http://localhost:5000/webhook/'
 
 # Connexion à la base de données
 conn = sqlite3.connect('refresh.db')
@@ -25,25 +26,32 @@ cursor.execute('''
 # Commencez la boucle infinie pour exécuter le script toutes les 30 secondes
 while True:
     # Sélection des lignes où etat = 0
-    cursor.execute('SELECT id_bnb, days FROM refresh WHERE etat = 0')
+    cursor.execute('SELECT id_bnb, days, id FROM refresh WHERE etat = 0')
     rows = cursor.fetchall()
     print(rows)
 
-    for id_bnb, days in rows:
+    for id_bnb, days, id in rows:
         # Construisez la commande comme avant
         command = ["python", "airbnb.py", "--id", str(id_bnb), "--days", str(days)]
+        webhook_url = 'http://sonarbnb.com/scrap/{}'.format(id)
+        print(webhook_url)
+
         
         # Exécutez la commande avec subprocess.run
         result = subprocess.run(command)
 
         if result.returncode == 0:
-            cursor.execute('UPDATE refresh SET etat = 1 WHERE id_bnb = ?', (id_bnb,))
+            cursor.execute('UPDATE refresh SET etat = 1 WHERE id = ?', (id,))
+            response = requests.get(webhook_url)
         else:
-            cursor.execute('UPDATE refresh SET etat = 2 WHERE id_bnb = ?', (id_bnb,))
+            cursor.execute('UPDATE refresh SET etat = 2 WHERE id = ?', (id,))
+
+
+
         conn.commit()
 
 
-    time.sleep(20)
+    time.sleep(10)
 conn.commit()
 cursor.close()
 conn.close()
