8222bdd0699c04d47e4c95c43552974d373d4bf1
louis-git-hub <107806927+louis-git-hub@users.noreply.github.com>
2024-03-01 22:53:47 +0100

    Update airbnb.py

--- airbnb.py
+++ airbnb.py
@@ -6,10 +6,12 @@ import re  # Assurez-vous d'importer le module re pour extract_number_from_strin
 
 # Initialisation des arguments du script
 parser = argparse.ArgumentParser(description='Traite les variables DAYS et ID.')
-parser.add_argument('--days', type=int, default=30, help='Nombre de jours')
-parser.add_argument('--id', type=str, required=True, help='Identifiant unique')  # ID est requis
+parser.add_argument('--days', type=int, default=7, help='Nombre de jours')
+parser.add_argument('--id', type=str, required=True, help='Identifiant unique')
+parser.add_argument('--idr', type=str, required=True, help='Identifiant unique')
 args = parser.parse_args()
 
+IDR = args.idr
 DAYS = args.days
 ID = args.id
 BASE = "https://www.airbnb.fr/rooms/"
@@ -27,7 +29,7 @@ def extract_number_from_string(text):
 
 # Préparation du nom du fichier CSV
 today_str = datetime.now().strftime("%Y-%m-%d")
-filename_csv = f"{ID}_{DAYS}_{today_str}.csv"
+filename_csv = f"{IDR}_{ID}_{DAYS}_{today_str}.csv"
 
 # Ouverture du fichier CSV pour écriture
 with open(filename_csv, mode='w', newline='', encoding='utf-8') as file:
@@ -38,45 +40,47 @@ with open(filename_csv, mode='w', newline='', encoding='utf-8') as file:
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto(URL, wait_until='networkidle')
-        if page.is_visible(close_button_selector):
-            page.click(close_button_selector)
-        for i in range(DAYS):
-            page.click(fermer)
-            target_date = datetime.now() + timedelta(days=i)
-            target_date_str = target_date.strftime("%d/%m/%Y")
-            if page.is_visible(f'[data-testid="calendar-day-{target_date_str}"]') and \
-               page.is_enabled(f'[data-testid="calendar-day-{target_date_str}"]'):
-                page.click(f'[data-testid="calendar-day-{target_date_str}"]')
-                page.wait_for_timeout(2000)
-            else:
-                writer.writerow(["Bloqué", target_date_str, 0])
-                continue
-            nombre = page.inner_text(testid_selector)
-            b_nombre = extract_number_from_string(nombre) if nombre else 0
-            a_nombre = b_nombre if b_nombre > 0 else 1
-            tomorrow_date = target_date + timedelta(days=a_nombre)
-            tomorrow_date_str = tomorrow_date.strftime("%d/%m/%Y")
-            if page.is_visible(f'[data-testid="calendar-day-{tomorrow_date_str}"]') and \
-               page.is_enabled(f'[data-testid="calendar-day-{tomorrow_date_str}"]'):
-                page.click(f'[data-testid="calendar-day-{tomorrow_date_str}"]')
-            else:
-                writer.writerow(["Bloqué", target_date_str, 0])
-                continue
-            section_element = page.query_selector(ppp)
-            if section_element:
-                price_element = section_element.query_selector(tot)
-                if price_element:
-                    price_text = extract_number_from_string(price_element.inner_text())
-                    if b_nombre > 0:
-                        price_sql = price_text / b_nombre
-                    else:
-                        price_sql = price_text
-                    writer.writerow([price_sql, target_date_str, b_nombre])
+        try:
+            page.goto(URL, wait_until='networkidle')
+            if page.is_visible(close_button_selector):
+                page.click(close_button_selector)
+            for i in range(DAYS):
+                page.click(fermer)
+                target_date = datetime.now() + timedelta(days=i)
+                target_date_str = target_date.strftime("%d/%m/%Y")
+                if page.is_visible(f'[data-testid="calendar-day-{target_date_str}"]') and \
+                page.is_enabled(f'[data-testid="calendar-day-{target_date_str}"]'):
+                    page.click(f'[data-testid="calendar-day-{target_date_str}"]')
+                    page.wait_for_timeout(2000)
                 else:
                     writer.writerow(["Bloqué", target_date_str, 0])
                     continue
-            else:
-                writer.writerow(["Bloqué", target_date_str, 0])
-                continue
-    browser.close()
+                nombre = page.inner_text(testid_selector)
+                b_nombre = extract_number_from_string(nombre) if nombre else 0
+                a_nombre = b_nombre if b_nombre > 0 else 1
+                tomorrow_date = target_date + timedelta(days=a_nombre)
+                tomorrow_date_str = tomorrow_date.strftime("%d/%m/%Y")
+                if page.is_visible(f'[data-testid="calendar-day-{tomorrow_date_str}"]') and \
+                page.is_enabled(f'[data-testid="calendar-day-{tomorrow_date_str}"]'):
+                    page.click(f'[data-testid="calendar-day-{tomorrow_date_str}"]')
+                else:
+                    writer.writerow(["Bloqué", target_date_str, 0])
+                    continue
+                section_element = page.query_selector(ppp)
+                if section_element:
+                    price_element = section_element.query_selector(tot)
+                    if price_element:
+                        price_text = extract_number_from_string(price_element.inner_text())
+                        if b_nombre > 0:
+                            price_sql = price_text / b_nombre
+                        else:
+                            price_sql = price_text
+                        writer.writerow([price_sql, target_date_str, b_nombre])
+                    else:
+                        writer.writerow(["Bloqué", target_date_str, 0])
+                        continue
+                else:
+                    writer.writerow(["Bloqué", target_date_str, 0])
+                    continue
+        finally:
+            browser.close()
