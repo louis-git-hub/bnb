1a2e14162f4e98bca3bfb4fb709ff1649ad2a426
louis-git-hub <107806927+louis-git-hub@users.noreply.github.com>
2024-03-03 17:02:57 +0100

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -2,6 +2,7 @@ import sqlite3
 import subprocess
 import time
 import requests
+from datetime import datetime
 
 # Chemin vers le script secondaire que vous souhaitez exécuter
 script_path = 'airbnb.py'
@@ -10,6 +11,13 @@ webhook_url = 'http://localhost:5000/webhook/'
 # Connexion à la base de données
 conn = sqlite3.connect('refresh.db')
 cursor = conn.cursor()
+
+# def get_current_datetime():
+#     # Récupérer la date et l'heure actuelles
+#     current_datetime = datetime.now()
+#     # Formater la date et l'heure actuelles pour inclure les secondes
+#     formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
+#     return formatted_datetime
 
 # Création de la table si elle n'existe pas (pas de nouvelle insertion ici pour éviter les doublons)
 cursor.execute('''
@@ -32,22 +40,28 @@ while True:
 
     for id_bnb, days, id in rows:
         # Construisez la commande comme avant
-        command = ["python", "airbnb.py", "--id", str(id_bnb), "--id", str(id), "--days", str(days)]
+        command = ["python", "airbnb.py", "--idr", str(id), "--id", str(id_bnb), "--days", str(days)]
         webhook_url = 'http://sonarbnb.com/scrap/{}'.format(id)
+        webhook_url_error = 'http://sonarbnb.com/scrap/error/{}'.format(id)
         print(webhook_url)
 
-        
-        # Exécutez la commande avec subprocess.run
+
+        date_d = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute('UPDATE refresh SET date_d = ? WHERE id = ?', (date_d, id))
+        cursor.execute('UPDATE refresh SET etat = 1 WHERE id = ?', (id,))
+        conn.commit()
         result = subprocess.run(command)
 
         if result.returncode == 0:
-            cursor.execute('UPDATE refresh SET etat = 1 WHERE id = ?', (id,))
+            date_f = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cursor.execute('UPDATE refresh SET etat = 2 WHERE id = ?', (id,))
+            cursor.execute('UPDATE refresh SET date_f = ? WHERE id = ?', (date_f, id))
             response = requests.get(webhook_url)
         else:
-            cursor.execute('UPDATE refresh SET etat = 2 WHERE id = ?', (id,))
-
-
-
+            date_f = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cursor.execute('UPDATE refresh SET etat = 3 WHERE id = ?', (id,))
+            cursor.execute('UPDATE refresh SET date_f = ? WHERE id = ?', (date_f, id))
+            response = requests.get(webhook_url_error)
         conn.commit()
 
 
