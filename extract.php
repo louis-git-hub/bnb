bef447cc93c519f7104feb69bec54c23346806fb
baudryj <julien.baudry@gmail.com>
2024-03-03 16:50:03 +0100

    Simplification .fr .ch ... etc

--- extract.php
+++ extract.php
@@ -1,14 +1,12 @@
 <?php
 
-function url_to_id(): string {
-    $url = "https://www.airbnb.fr/rooms/31557923?adults=1&category_tag=Tag%3A7769&children=0&enable_m3_private_room=true&infants=0&pets=0&photo_id=912108901&search_mode=flex_destinations_search&check_in=2024-03-21&check_out=2024-03-26&source_impression_id=p3_1709314663_PmgiNVipMjFVLzrO&previous_page_section_name=1000&federated_search_id=9aa5d38a-9053-47c3-b9eb-07a99885591c";
-
+function url_to_id(string $url): string {
     // Utilisation d'une expression régulière pour extraire l'ID
-    if (preg_match('/airbnb\.fr\/rooms\/(\d+)/', $url, $matches)) {
+    if (preg_match('/airbnb.+?\/rooms\/(\d+)/', $url, $matches)) {
         // L'ID est le premier groupe capturé
         return $matches[1]; // Retourne l'ID si trouvé
     } else {
-        return ''; // Retourne une chaîne vide si aucun ID n'est trouvé
+        return ""; // Retourne une chaîne vide si aucun ID n'est trouvé
     }
 }
 
