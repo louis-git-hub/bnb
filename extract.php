45cb5aaed3d03c0bee91b0e804bcb358c4853907
louis-git-hub <107806927+louis-git-hub@users.noreply.github.com>
2024-03-03 16:20:53 +0100

    Update extract.php

--- extract.php
+++ extract.php
@@ -1,7 +1,7 @@
 <?php
 
 function url_to_id(): string {
-    $url = "https://www.airbnb.fr/rooms/33463196?adults=1&category_tag=Tag%3A7769&children=0&enable_m3_private_room=true&infants=0&pets=0&photo_id=912108901&search_mode=flex_destinations_search&check_in=2024-03-21&check_out=2024-03-26&source_impression_id=p3_1709314663_PmgiNVipMjFVLzrO&previous_page_section_name=1000&federated_search_id=9aa5d38a-9053-47c3-b9eb-07a99885591c";
+    $url = "https://www.airbnb.fr/rooms/31557923?adults=1&category_tag=Tag%3A7769&children=0&enable_m3_private_room=true&infants=0&pets=0&photo_id=912108901&search_mode=flex_destinations_search&check_in=2024-03-21&check_out=2024-03-26&source_impression_id=p3_1709314663_PmgiNVipMjFVLzrO&previous_page_section_name=1000&federated_search_id=9aa5d38a-9053-47c3-b9eb-07a99885591c";
 
     // Utilisation d'une expression régulière pour extraire l'ID
     if (preg_match('/airbnb\.fr\/rooms\/(\d+)/', $url, $matches)) {
@@ -13,9 +13,8 @@ function url_to_id(): string {
 }
 
 $result = url_to_id();
-$days = 7 //c une valeur par défaut
 
-$url = "http://127.0.0.1:5000/api/" . $result . $days;
+$url = "http://127.0.0.1:5000/api/" . $result . "/4";
 
 echo $url; // Ajoutez un point-virgule ici
 
