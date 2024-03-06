12aeb57e629852eb8aaf77ff14e192d45b6132c9
baudryj <julien.baudry@gmail.com>
2024-03-06 13:43:18 +0100

    Ajout get CSV from Id
    Ajout des lib os, glob, flask ...
    Modification port par défaut 8089

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, send_from_directory, abort
+import os
+import glob
+
+# Variables
+# Remplacez ceci par le chemin de votre répertoire où les fichiers CSV sont stockés
+CSV_FOLDER = '/home/py/scraper1/data'
 
 app = Flask(__name__)
 
@@ -28,5 +34,23 @@ def get_data(id_bnb, days):
         conn.commit()  # Les modifications sont automatiquement sauvegardées
     return f"{id_bnb},{days}"
 
+# Get csv from id task
+@app.route('/api/csv/<id>')
+def download_csv(id):
+    # Utilisez glob pour trouver des fichiers qui correspondent au modèle ID_*.csv
+    pattern = os.path.join(CSV_FOLDER, f'{id}_*.csv')
+    matching_files = glob.glob(pattern)
+    
+    # Vérifiez s'il y a au moins un fichier correspondant
+    if not matching_files:
+        # Si aucun fichier correspondant n'est trouvé, retournez une erreur 404
+        abort(404)
+    
+    # Si des fichiers correspondants existent, prenez le premier (ou un autre critère spécifique si nécessaire)
+    filename = os.path.basename(matching_files[0])
+    
+    # Retournez le fichier CSV
+    return send_from_directory(CSV_FOLDER, filename, as_attachment=True)
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(port=8089, debug=False)
